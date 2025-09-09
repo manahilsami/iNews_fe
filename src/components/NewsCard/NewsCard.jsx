6ffab172a1e516187ecbe875abab2eb3d2a1eee0
Manahil Sami <manahil_sami@outlook.com>
2025-09-09 09:00:37 -0400

    NewsCard component created

---
+++ src/components/NewsCard/NewsCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./NewsCard.css";
+
+function NewsCard() {
+  return (
+    <div className="news-card">
+      <img className="news-card__image" src="" alt="Article Image" />
+      <button className="news-card__bookmark"></button>
+      <button className="news-card__bookmark-colored"></button>
+      <p className="news-card__date">Date</p>
+      <h2 className="news-card__title">News Title</h2>
+      <p className="news-card__description">News description goes here.</p>
+      <p className="news-card__source">News Source</p>
+    </div>
+  );
+}
+
+export default NewsCard;
