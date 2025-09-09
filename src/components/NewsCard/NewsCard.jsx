38f51a9a936b8817a23466c2d8d44665a7ff23b1
Manahil Sami <manahil_sami@outlook.com>
2025-09-09 09:10:04 -0400

    NewsCard edit

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -7,10 +7,12 @@ function NewsCard() {
       <img className="news-card__image" src="" alt="Article Image" />
       <button className="news-card__bookmark"></button>
       <button className="news-card__bookmark-colored"></button>
-      <p className="news-card__date">Date</p>
-      <h2 className="news-card__title">News Title</h2>
-      <p className="news-card__description">News description goes here.</p>
-      <p className="news-card__source">News Source</p>
+      <div className="news-card__content">
+        <p className="news-card__date">Date</p>
+        <h2 className="news-card__title">News Title</h2>
+        <p className="news-card__description">News description goes here.</p>
+        <p className="news-card__source">News Source</p>
+      </div>
     </div>
   );
 }
