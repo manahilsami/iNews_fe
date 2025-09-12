68a8e75ef1f5b3f49e1c17ffc45775a2a0f23578
Manahil Sami <manahil_sami@outlook.com>
2025-09-12 08:51:41 -0400

    modals close on esc

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,10 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./ModalWithForm.css";
 
 function ModalWithForm({ title, isOpen, onClose, onSubmit, children }) {
+  useEffect(() => {
+    const handleEscClose = (e) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener("keydown", handleEscClose);
+    }
+
+    return () => {
+      document.removeEventListener("keydown", handleEscClose);
+    };
+  }, [isOpen, onClose]);
+
   return (
     <div className={`modal ${isOpen ? "modal_opened" : ""}`} onClick={onClose}>
+      {" "}
+      {/* clicking overlay outside of modal closes it */}
       <div className="modal__content" onClick={(e) => e.stopPropagation()}>
+        {" "}
+        {/* prevents closing when clicking inside modal */}
         <button className="modal__close" type="button" onClick={onClose} />
         <h2 className="modal__title">{title}</h2>
         <form className="modal__form" onSubmit={onSubmit}>
