85d803fe0925e725cbe1cd3774fe81ea6db58c95
Manahil Sami <manahil_sami@outlook.com>
2025-09-15 09:14:09 -0400

    initial auth.js file created

---
+++ src/utils/auth.js
@@ -0,0 +1,47 @@
+import { checkResponse } from "./api.js";
+
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://api.wtwrms.jumpingcrab.com"
+    : "http://localhost:3001";
+
+export const signup = ({ username, email, password }) => {
+  return fetch(`${baseUrl}/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, email, password }),
+  }).then(checkResponse);
+};
+
+export const signin = ({ email, password }) => {
+  return fetch(`${baseUrl}/signin`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then(checkResponse);
+};
+
+export const checkToken = (token) => {
+  return fetch(`${baseUrl}/users/me`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+  }).then(checkResponse);
+};
+
+export const updateProfile = ({ name, avatar }, token) => {
+  return fetch(`${baseUrl}/users/me`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ name, avatar }),
+  }).then(checkResponse);
+};
