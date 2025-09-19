74c5438019df8cc751d07e50139bc8ebc01dca23
Manahil Sami <manahil_sami@outlook.com>
2025-09-19 13:39:11 -0400

    login/register modal styles edited

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,14 @@ import "./Header.css";
 import React from "react";
 import logo from "../../images/NewsExplorer.svg";
 import Navigation from "../Navigation/Navigation";
+import { Link } from "react-router-dom";
 
 function Header({ onSignInClick }) {
   return (
     <header className="header">
-      <img className="header__logo" alt="NewsExplorer Logo" src={logo} />
+      <Link to="/">
+        <img className="header__logo" alt="NewsExplorer Logo" src={logo} />
+      </Link>
       <div className="header__right">
         <Navigation onSignInClick={onSignInClick} />
       </div>
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,16 @@
-
 import "./Navigation.css";
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navigation({ onSignInClick }) {
   return (
     <div className="navigation">
-      <button className="navigation__home">Home</button>
-      <button className="navigation__signin" onClick={onSignInClick}>Sign In</button>
+      <Link to="/">
+        <button className="navigation__home">Home</button>
+      </Link>
+      <button className="navigation__signin" onClick={onSignInClick}>
+        Sign In
+      </button>
     </div>
   );
 }
