429a72f37ab2e0c44297c18f86f5d27bfdfd5a4d
Manahil Sami <manahil_sami@outlook.com>
2025-09-24 00:41:28 -0400

    fix: page underline

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,20 +2,32 @@ import "./Navigation.css";
 import React from "react";
 import logoutIconDark from "../../images/logout.svg";
 import logoutIconWhite from "../../images/logout-white.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Navigation({ onSignInClick, user, onLogout, isSaved }) {
-  // On home (isSaved=false, dark background), use white icon; on saved (isSaved=true, light background), use dark icon
   const logoutIcon = isSaved ? logoutIconDark : logoutIconWhite;
+  const { pathname } = useLocation();
+  const onHome = pathname === "/";
+  const onSaved = pathname === "/saved-news";
   return (
     <div className="navigation">
       <Link to="/">
-        <button className="navigation__home">Home</button>
+        <button
+          className={`navigation__home ${onHome ? "navigation__active" : ""}`}
+        >
+          Home
+        </button>
       </Link>
       {user ? (
         <>
           <Link to="/saved-news">
-            <button className="navigation__saved">Saved articles</button>
+            <button
+              className={`navigation__saved ${
+                onSaved ? "navigation__active" : ""
+              }`}
+            >
+              Saved articles
+            </button>
           </Link>
           <div className="navigation__user-wrap">
             <div className="navigation__user-btn">
